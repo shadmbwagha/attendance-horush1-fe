511114687a7f58731ca3cff6dcf241e866a113fa
shad mbwagha <shadracksylvestar@gmail.com>
2024-02-07 07:31:42 +0300

    firt push attendance

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable no-multiple-empty-lines -->
+<template>
+  <h1></h1>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView'
+
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable quotes */
+module.exports = {
+  content: [
+    './index.html',
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
